09a43445710d416235f19a25e2c11e7bb26c8fde
hazreta <hazreta.tekeoglu@gmail.com>
2022-01-11 09:06:07 -0500

    work 3 is done=>commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,9 @@ public class HelloWorld {
     // Doing some work here
 
     // Doing awesome work2 !
+
+    // Doing awesome work 3!
+    // Awesome work 3 is done and ready to be committed and ready to be pushed to remote repository
+
 }
+
